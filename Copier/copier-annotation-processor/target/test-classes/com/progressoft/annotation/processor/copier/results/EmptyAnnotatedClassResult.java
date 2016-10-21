6be7d7f49230778e16602c954ba73869e236af2b
Ahmad Bawaneh <akabme@gmail.com>
2016-10-21 16:04:56 +0300

    refactoring

--- Copier/copier-annotation-processor/target/test-classes/com/progressoft/annotation/processor/copier/results/EmptyAnnotatedClassResult.java
+++
@@ -1,9 +0,0 @@
-package com.progressoft.annotation.processor.copier;
-
-class EmptyAnnotatedClassCopier {
-
-    EmptyAnnotatedClass copy(EmptyAnnotatedClass original) throws CloneNotSupportedException {
-        EmptyAnnotatedClass result=new EmptyAnnotatedClass();
-        return result;
-    }
-}
--- Copier/copier-annotation-processor/target/test-classes/com/progressoft/annotation/processor/copier/results/SecondEmptyAnnotatedClassResult.java
+++
@@ -1,9 +0,0 @@
-package com.progressoft.annotation.processor.copier;
-
-class SecondEmptyAnnotatedClassCopier {
-
-    SecondEmptyAnnotatedClass copy(SecondEmptyAnnotatedClass original) throws CloneNotSupportedException {
-        SecondEmptyAnnotatedClass result=new SecondEmptyAnnotatedClass();
-        return result;
-    }
-}
