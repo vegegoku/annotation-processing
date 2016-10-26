a5186be4ddc60914b6cb40c74e298f169babe41d
Ahmad Bawaneh <akabme@gmail.com>
2016-10-26 10:00:12 +0300

    Handling inner classes

--- annotation-processing/processor-testing/src/main/java/com/progressoft/jfw/annotations/processor/test/TargetProcessor.java
+++ annotation-processing/processor-testing/src/main/java/com/progressoft/jfw/annotations/processor/test/TargetProcessor.java
@@ -8,10 +8,12 @@ import javax.annotation.processing.Processor;
 import javax.tools.JavaFileObject;
 import javax.tools.StandardLocation;
 import java.nio.charset.Charset;
+import java.util.HashSet;
+import java.util.Set;
 
 import static com.google.testing.compile.JavaSourceSubjectFactory.javaSource;
 
-public class TargetProcessor{
+public class TargetProcessor {
 
     private final String inputClassName;
     private final Processor processor;
@@ -22,29 +24,26 @@ public class TargetProcessor{
         this.processor = processor;
     }
 
-    public void generates(String result, String... rest){
-        if(rest !=null && rest.length>0){
-            JavaFileObject[] files=new JavaFileObject[rest.length];
-            for(int i=0;i<rest.length;i++){
-                files[i]=JavaFileObjects.forSourceString("",rest[i]);
-            }
-            compilesWithoutErrors()
-                    .and()
-                    .generatesSources(JavaFileObjects.forSourceString("",result), files);
-        }else{
-            compilesWithoutErrors()
-                    .and()
-                    .generatesSources(JavaFileObjects.forSourceString("",result));
-        }
+    public void generates(String result, String... rest) {
+        compilesWithoutErrors()
+                .and()
+                .generatesSources(JavaFileObjects.forSourceString("", result), asJavaFileObjectsArray(rest));
 
     }
 
-    public CompileTester.SuccessfulCompilationClause compilesWithoutErrors(){
+    private JavaFileObject[] asJavaFileObjectsArray(String... rest) {
+        JavaFileObject[] result = new JavaFileObject[rest.length];
+        for (int i = 0; i < rest.length; i++)
+            result[i] = JavaFileObjects.forSourceString("", rest[i]);
+        return result;
+    }
+
+    public CompileTester.SuccessfulCompilationClause compilesWithoutErrors() {
         return Truth.assert_().about(javaSource()).that(JavaFileObjects.forResource(inputClassName))
                 .processedWith(processor).compilesWithoutError();
     }
 
-    public void generatesResource(String basePackage, String fileName, String content){
+    public void generatesResource(String basePackage, String fileName, String content) {
         compilesWithoutErrors()
                 .and()
                 .generatesFileNamed(StandardLocation.SOURCE_OUTPUT, basePackage, fileName).withStringContents(Charset.defaultCharset(), content);
